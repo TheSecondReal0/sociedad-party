f0d8d3c086b7b23ac70eda3d2229b50ca9a615b9
JamesSprow <103532478+JamesSprow@users.noreply.github.com>
2023-08-09 21:34:09 -0400

    implement moving and adding cards to stack

--- src/games/stack_wars/stack/stack.gd
+++ src/games/stack_wars/stack/stack.gd
@@ -5,8 +5,9 @@ class_name Stack
 var cards: Array[Card] = []
 
 
-func add_card(card: Card):
-	cards.append(card)
+func add_card(card: Card, index: int = -1):
+	if index == -1:
+		cards.append(card)
 
 func remove_card(card: Card):
 	cards.erase(card)
@@ -15,6 +16,10 @@ func resolve_next_card():
 	var card: Card = cards.back()
 	cards.pop_back()
 	card.resolve()
+
+func move_card(card: Card, index: int):
+	cards.erase(card)
+	cards.insert(index, card)
 
 func reverse():
 	cards.reverse()
