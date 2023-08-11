b9ed78a1f6f28f789ce74c091765473b78108223
TheSecondReal0 <66881186+TheSecondReal0@users.noreply.github.com>
2023-08-10 21:03:18 -0400

    ui signal stuff

--- src/games/stack_wars/ui/card/card.gd
+++ src/games/stack_wars/ui/card/card.gd
@@ -7,8 +7,22 @@ var card: StackWars_Card = null
 @export var description: RichTextLabel
 @export var turn_constraint: Label
 
+signal selected
+
 func init_card(_card: StackWars_Card) -> void:
 	card = _card
 	title.text = card.title
 	description.text = card.description
-	turn_constraint.text = card.turn_constraint
+	turn_constraint.text = card.turn_constraint_num as String
+
+
+
+func _on_gui_input(event: InputEvent):
+	if event.is_action("ui_select"):
+		selected.emit()
+	if not event is InputEventMouseButton:
+		return
+	event = event as InputEventMouseButton
+	if event.button_mask != MOUSE_BUTTON_LEFT or not event.pressed:
+		return
+	selected.emit()
--- src/games/stack_wars/ui/stack/stack.gd
+++ src/games/stack_wars/ui/stack/stack.gd
@@ -1,11 +1,8 @@
 extends ScrollContainer
 
+signal card_selected(card: StackWars_Card)
+signal card_deselected
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
